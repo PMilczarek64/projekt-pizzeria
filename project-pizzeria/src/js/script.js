b358594c3e0f312968050740fd02ab1e3c22a756
Mammoth64 <mammoth1907@gmail.com>
2022-02-25 18:44:59 +0100

    Generate HTML based on template

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -51,8 +51,43 @@
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
+  
+  class Product {
+    constructor(id, data){
+      const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+      console.log('new Product: ', thisProduct);
+    }
+    renderInMenu(){
+      const thisProduct = this;
+      // Generate a HTML code based on template
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      // Create element using utils.createElementFromHTML 
+      // Find a MENU container in the website.
+      // add created DOM element to MENU container.
+
+    }
+  }
 
   const app = {
+    initMenu: function(){
+      const thisApp = this;
+
+      console.log('thisApp.data: ', thisApp.data);
+      
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,8 +95,10 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
-
   app.init();
 }
